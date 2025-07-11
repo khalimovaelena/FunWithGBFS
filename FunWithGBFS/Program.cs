9fa9c65a2ec33089f90f2d77f36fc9a3fe515798
HOME_WINDOW\helen <helenkhalimova@gmail.com>
2025-07-11 18:07:01 +0200

    Fetch data from test APIs

---
+++ FunWithGBFS/Program.cs
@@ -0,0 +1,22 @@
+﻿using FunWithGBFS;
+
+internal class Program
+{
+    public static async Task Main(string[] args)
+    {
+        //TODO: appsettings
+        var providers = new List<Provider>
+            {
+                new Provider("BlueBike_BE", "https://api.delijn.be/gbfs/gbfs.json"),
+                new Provider("Check_Rotterdam", "https://api.ridecheck.app/gbfs/v3/rotterdam/gbfs.json"),
+                new Provider("Felyx_Delft", "https://maas.zeus.cooltra.com/gbfs/delft/3.0/en/gbfs.json")
+            };
+
+        foreach (var provider in providers)
+        {
+            Console.WriteLine($"\nFetching data for {provider.City}...");
+            var stations = await GbfsFetcher.FetchGbfsData(provider);
+            Console.WriteLine($"Stations found: {stations.Count}");
+        }
+    }
+}
---
+++ FunWithGBFS/Provider.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FunWithGBFS
+{
+    public class Provider
+    {
+        public string City { get; set; }
+        public string GbfsUrl { get; set; }
+
+        public Provider(string city, string gbfsUrl)
+        {
+            City = city;
+            GbfsUrl = gbfsUrl;
+        }
+    }
+}
